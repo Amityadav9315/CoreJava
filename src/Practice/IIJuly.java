d999b1999c3f47e28a2393ffbf31829b6452e23d
Amit Yadav <amit931552@gmail.com>
2025-07-02 09:33:02 +0530

    sgvbksf

---
+++ src/Practice/IIJuly.java
@@ -0,0 +1,38 @@
+package Practice;
+
+public class IIJuly {
+
+    String meth1(int a,int b){
+        System.out.println(10);
+        System.out.println((54+a)-b);
+        int value=new IIJuly().meth5();
+        System.out.println(value);
+        return "Java";
+
+    }
+    int meth2(int a){
+        System.out.println(75);
+        int b=5;
+        System.out.println(b-a);
+        return (5*5)-b;
+
+    }
+    void meth3(){
+        System.out.println("Start");
+        System.out.println(new IIJuly().meth4());
+        System.out.println(40);
+
+    }
+
+    String meth4(){
+        System.out.println();
+
+    }
+    int meth5(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
